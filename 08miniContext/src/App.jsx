b65f77f12b76cd041350edc4cc0ee654f13bb818
satyam chouksey <satyamchouksey007@gmail.com>
2024-06-11 19:37:32 +0530

    Implement Todo Application using Context Api

--- 08miniContext/src/App.jsx
+++ 08miniContext/src/App.jsx
@@ -1,11 +1,9 @@
-import { useState } from 'react'
 import './App.css'
 import UserContextProvider from './context/UserContextProvider'
 import Login from './components/Login'
 import Profile from './components/Profile'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <UserContextProvider>
